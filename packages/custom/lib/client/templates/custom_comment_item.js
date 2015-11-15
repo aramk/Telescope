b0425cbce1e0c293d63b0c80a2b50da7a91770ea
Aram Kocharyan <akarmenia@gmail.com>
2015-11-15 19:55:25 +1100

    Use baseScore for comments
    - Displayed baseScore instead of upvotes for comments.
    - Sorted by baseScore instead of upvotes for comments.

--- packages/custom/lib/client/templates/custom_comment_item.js
+++ packages/custom/lib/client/templates/custom_comment_item.js
@@ -3,5 +3,10 @@ TemplateClass = Template.comment_item;
 TemplateClass.helpers({
   isTopComment: function() {
     return this.first && this.upvotes > 1;
+  },
+  points: function() {
+    // Upvotes are displayed by default, but this will display the overall score
+    // (upvotes - downvotes).
+    return this.baseScore;
   }
 });
