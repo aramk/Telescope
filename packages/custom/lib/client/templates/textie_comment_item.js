6868de255ba9c4cd4f9d7f96fbec85eb8c157156
Aram Kocharyan <akarmenia@gmail.com>
2015-11-25 10:35:37 +1100

    Style and reply vote changes
    - Removed user votes from their replies on user replies page.
    - Wrapped category buttons.
    - Fixed margin on call to action.
    - Reduced mobile header size to fit more messages.

--- packages/custom/lib/client/templates/textie_comment_item.js
+++ packages/custom/lib/client/templates/textie_comment_item.js
@@ -1,16 +1,29 @@
 var TemplateClass = Template.comment_item;
 
+TemplateClass.onCreated(function() {
+  // Base score with user votes ignored.
+  this.userBaseScore = this.data.baseScore;
+  var user = Meteor.user();
+  if (user) {
+    if (_.contains(this.data.upvoters, user._id)) {
+      this.userBaseScore -= getVotePower(user);
+    } else if (_.contains(this.data.downvoters, user._id)) {
+      this.userBaseScore += getVotePower(user);
+    }
+  }
+});
+
 TemplateClass.helpers({
   zeroBaseScore: function() {
-    return this.baseScore === 0;
+    return Template.instance().userBaseScore === 0;
   },
   oneAbsBaseScore: function() {
-    return Math.abs(this.baseScore) === 1;
+    return Math.abs(Template.instance().userBaseScore) === 1;
   },
   scoreThumbClass: function() {
-    return this.baseScore < 0 ? 'down' : 'up';
+    return Template.instance().userBaseScore < 0 ? 'down' : 'up';
   },
   negativeBaseScore: function() {
-    return this.baseScore < 0;
+    return Template.instance().userBaseScore < 0;
   }
 });
--- packages/custom/lib/client/templates/textie_postsLoadMore.js
+++ packages/custom/lib/client/templates/textie_postsLoadMore.js
@@ -3,6 +3,9 @@ var TemplateClass = Template.postsLoadMore;
 TemplateClass.helpers({
   isUserPosts: function() {
     return this.controllerInstance.terms.get().view === 'userPosts';
+  },
+  categoriesPage: function() {
+    return this.controllerInstance.terms.get().view === 'categoryPosts';
   }
 });
 
